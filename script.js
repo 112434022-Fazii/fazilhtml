13f257a0bfbb487066369e26b0a4e2bc875a9727
112434022-Fazii <112434022@kanchiuniv.ac.in>
2026-08-16 22:51:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function selectMood(mood) {
+function bookTicket() {
 
-    document.getElementById("result").innerHTML =
-    "<h2>You selected: " + mood + "</h2>";
+    let movie = document.getElementById("movie").value;
+    let date = document.getElementById("date").value;
+    let time = document.getElementById("time").value;
+    let tickets = document.getElementById("tickets").value;
 
+    if (movie == "" || date == "" || time == "") {
+
+        alert("Please fill all details");
+
+    } else {
+
+        alert("Booking Successful!");
+
+        window.location.href = "confirmation.html";
+    }
 }
